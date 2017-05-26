5fcb952ff03b0f18885a6df3113dd1725ca0fcf3
Paul Lee <paullee@Pauls-MBP.lan>
2017-05-26 10:02:01 -0400

    ThirdPartyPractice

--- startupadvice/src/App.js
+++ startupadvice/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar'
 
-var quotes = ["Get some fucking exercise", "Solve a real fucking problem", "Do fucking one on one's", "Stop fucking around and get back to work", "Act like you know what you're fucking doing"];
+
+
+
+var quotes = ["Get some fucking exercise", "Solve a real fucking problem", "Do fucking one on one's", "Stop fucking around and get back to work", "Act like you know what you're fucking doing", "Get a fucking lawyer"];
 class App extends Component {
   constructor (props) {
     super (props);
@@ -13,7 +18,7 @@ class App extends Component {
   }
 
   rotateQuotes() {
-    if( this.state.currentIndex === 4) {
+    if( this.state.currentIndex === 5) {
       this.setState( {
         attributes: quotes,
         currentIndex: 0,
@@ -26,14 +31,42 @@ class App extends Component {
     }
   }
 
+  rotateBack() {
+    if (this.state.currentIndex === 0) {
+      this.setState( {
+        attributes: quotes,
+        currentIndex: 5,
+      })
+    }
+    else {
+      this.setState( {
+        currentIndex: this.state.currentIndex - 1
+      })
+    }
+  }
+
+  rotateRandom() {
+    this.setState( {
+      currentIndex: Math.floor(Math.random() * 4)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
+          <MuiThemeProvider>
+            <AppBar title='About This Page'/>
+          </MuiThemeProvider>
           
           <h1>Great Fucking Startup Advice</h1>
         </div>
         <button className="App-button-quote" onClick={() => this.rotateQuotes()}>{quotes[this.state.currentIndex]}</button>
+        <button className="App-button-next" onClick={() => this.rotateBack()}>"Previous Quote"</button>
+        <button className="App-button-next" onClick={() => this.rotateQuotes()}>"Next Quote"</button>
+        <button className="App-button-random" onClick={() => this.rotateRandom()}>"Random quote"</button>
+        
       </div>
     );
   }
