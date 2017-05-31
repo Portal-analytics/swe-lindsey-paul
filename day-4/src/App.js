9627d55b1c09aa220f82e45cdc3bb2bc45143d69
linshavers <linshavers@gmail.com>
2017-05-31 10:42:41 -0400

    Sixth Challenge Work - Not Complete

--- day-4/src/App.js
+++ day-4/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import TextField from 'material-ui/TextField';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import FlatButton from 'material-ui/FlatButton';
 
-//injectTapEventPlugin();
 
 let button;
 let contractName;
@@ -15,21 +13,18 @@ let price;
 var index;
 var contractToEdit;
 
+var count = 0;
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = ({
       list: [],
-      contractName: [],
-      description: [],
-      price: [],
+      contractName: "",
+      description: "",
+      price: "",
 
-      newContractName: [],
-      newDescription: [],
-      newPrice: [],
-
-      index: 0,
       counter: 0,
+      index: 0,
 
     })
   }
@@ -47,46 +42,72 @@ class App extends Component {
       [field]: e.target.value,
     })
 
-    console.log(this.state.counter);
+    // console.log(this.state.counter);
   }
 
 
  handleSubmit() {
-   var Contract = [];
-   Contract.push(this.state.contractName);
-   Contract.push(this.state.description);
-   Contract.push(this.state.price);
-   Contract.push(this.state.index);
-   let newContracts = this.state.list.concat([Contract]);
-   this.setState({
-     ...this.state, 
-     list: newContracts,
-     index: this.state.index + 1,
-   })
+   var entryArr = this.state.list, 
+   updatedList = this.state.list.concat(
+      [
+        {contractName: this.state.contractName,
+          description: this.state.description,
+          price: this.state.price,
+          index: this.state.index +1}
+      ]);
 
-   console.log(this.state.list);
+    this.setState({
+      ...this.state, 
+      list: updatedList
+    })
+
+  //  //Challenge 4 
+  //  var Contract = [];
+  //  Contract.push(this.state.contractName);
+  //  Contract.push(this.state.description);
+  //  Contract.push(this.state.price);
+  //  Contract.push(this.state.index);
+  //  let newContracts = this.state.list.concat([Contract]);
+  //  this.setState({
+  //    ...this.state, 
+  //    list: newContracts,
+  //    index: this.state.index + 1,
+  //  })
+
+  //  console.log(this.state.list);
   }
 
   handleEdit(c) {
-    var index = c[3];
-    var contractToEdit = this.state.list[index];
-    console.log(index);
-    console.log(contractToEdit);
+    const updatedList = this.state.list.slice(0);
+    updatedList[c.index].isEditing = true;
+
+    this.setState({
+      ...this.state, 
+      list: updatedList,
+    })
 
   }
 
-  handleDone() {
-    contractToEdit[0] = this.state.newContractName;
-    contractToEdit[1] = this.state.newDescription;
-    contractToEdit[2] = this.state.newPrice;
+  handleDone(c) {
+    const updatedList= this.state.list.slice(0);
 
-    console.log(contractToEdit);
+    updatedList[c.index].isEditing = false;
+    updatedList[c.index].contractName = this.state.contractName;
+    updatedList[c.index].description = this.state.description;
+    updatedList[c.index].price = this.state.price;
+
+    this.setState({
+      ...this.state,  
+      list: updatedList
+    })
+
   }
 
 
 
   render() {
 
+    /*// Challenge 4
     const ContractList = this.state.list.map (
       (c) => {
         return (
@@ -94,6 +115,29 @@ class App extends Component {
              {"Contract Name:" + " " + c[0] + " " + "Description:" + " " + " " + c[1] + " " + "Price:" + c[2]}
              <button className= "edit-button" id= {this.state.counter} onClick= {() => this.handleEdit(c)}> Edit </button>
           </li>
+        )
+      }
+    )*/
+
+
+    //Challenge 5
+    const ContractList = this.state.list.map(
+      (c) => {
+        return(
+          <li>
+            Contract Name: { !c.isEditing && c.contractName + " "}
+            { c.isEditing && <input type="text" value={this.state.contractName} onChange={(e) => this.handleEditChange('contractName', e)}/> }
+            Description: { !c.isEditing && this.state.description + " "}
+            { c.isEditing && <input type="text" value={this.state.description} onChange={(e) => this.handleEditChange('description', e)}/> }
+            Price: { !c.isEditing && this.state.price + " "}
+            { c.isEditing && <input type="text" value={this.state.price} onChange={(e) => this.handleEditChange('price', e)}/> }
+
+            <button onClick= { () => {this.handleEdit(c)}}> Edit </button>
+            <button onClick= { () => {this.handleDone(c)}}>Done</button>
+
+
+          </li>  
+
         )
       }
     )
@@ -112,24 +156,10 @@ class App extends Component {
           <FlatButton label="Submit" onClick={() => this.handleSubmit()}/><br/>
         </div>
         <div className="App-contact-list">
-          Contract Name 
+          Contract List 
           <ul type="none">
             {ContractList}
           </ul>
-        </div>
-       
-        <div className="App-edit-window">   
-          Edit Window
-          <form>
-            <label>Contract Name:<input type="text" name="name" onChange={ (e) => this.handleEditChange('newContractName', e)} /></label>
-            <label>Description:<input type="text" name="description" onChange={ (e) => this.handleEditChange('newDescription', e)}/></label>
-            <label>price:<input type="text" name="price"onChange={ (e) => this.handleEditChange('newPrice', e)} /></label>
-          </form>
-          <button onclick={() => this.handleDone()}>Done</button>
-          {/*{this.state.newContractName}
-          {this.state.newDescription}
-          {this.state.newPrice}*/}
-          {contractToEdit}
         </div>
 
 
@@ -141,3 +171,14 @@ class App extends Component {
 }
 
 export default App;
+
+
+{/*<form>
+            <label>Contract Name:<input type="text" name="name" onChange={ (e) => this.handleEditChange('newContractName', e)} /></label>
+            <label>Description:<input type="text" name="description" onChange={ (e) => this.handleEditChange('newDescription', e)}/></label>
+            <label>price:<input type="text" name="price"onChange={ (e) => this.handleEditChange('newPrice', e)} /></label>
+          </form>
+          <button onclick={() => this.handleDone()}>Done</button>*/}
+          {/*{this.state.newContractName}
+          {this.state.newDescription}
+          {this.state.newPrice}*/}
